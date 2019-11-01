ad8435453eb8c05ef6de3d751b94e4c6d16930a0
klandrith007@gmail.com <klandrith007@gmail.com>
2019-11-01 15:32:51 -0700

    revised try-catch block

--- Project 3/inputoutput.hpp
+++ Project 3/inputoutput.hpp
@@ -60,76 +60,80 @@ public:
       // only execute block if current line in file is not empty string
       if (input.length() > 0) {
         command = input.at(0);
-        // switch block for various command inputs to execute related functions
-        // in DList
-        switch (command) {
-          // create list instance
-          case 'C':
-            create(list);
-            break;
-          // clear current list instance of nodes
-          case 'X':
-            clear(list);
-            break;
-          // delete current list instance
-          case 'D':
-            deletelist(list);
-            break;
-          // insert number into list in a sorted manner
-          case 'I':
-            insertSorted(input, list);
-            break;
-          // insert number at front of list
-          case 'F':
-            insertFront(input, list);
-            break;
-          // insert number at back of list
-          case 'B':
-            insertBack(input, list);
-            break;
-          // remove all of instances of a number
-          case 'E':
-            eliminateAllOf(input, list);
-            break;
-          // remove first instance of a number
-          case 'R':
-            eliminateFirstOf(input, list);
-            break;
-          // find a value in list
-          case 'G':
-            get(input, list);
-            break;
-          // output head node value
-          case 'A':
-            getHead(list);
-            break;
-          // output tail node value
-          case 'Z':
-            getTail(list);
-            break;
-          // remove head node
-          case 'T':
-            popHead(list);
-            break;
-          // remove tail node
-          case 'K':
-            popTail(list);
-            break;
-          // print size of list
-          case 'N':
-            getSize(list);
-            break;
-          // print list contents
-          case 'P':
-            printList(list);
-            break;
-          // commented line in input file
-          case '#':
-            break;
-          // unknown command in file
-          default:
-            cerr << "Invalid command detected in input file." << endl;
-            break;
+        try {
+          // switch block for various command inputs to execute related functions
+          // in DList
+          switch (command) {
+            // create list instance
+            case 'C':
+              create(list);
+              break;
+            // clear current list instance of nodes
+            case 'X':
+              clear(list);
+              break;
+            // delete current list instance
+            case 'D':
+              deletelist(list);
+              break;
+            // insert number into list in a sorted manner
+            case 'I':
+              insertSorted(input, list);
+              break;
+            // insert number at front of list
+            case 'F':
+              insertFront(input, list);
+              break;
+            // insert number at back of list
+            case 'B':
+              insertBack(input, list);
+              break;
+            // remove all of instances of a number
+            case 'E':
+              eliminateAllOf(input, list);
+              break;
+            // remove first instance of a number
+            case 'R':
+              eliminateFirstOf(input, list);
+              break;
+            // find a value in list
+            case 'G':
+              get(input, list);
+              break;
+            // output head node value
+            case 'A':
+              getHead(list);
+              break;
+            // output tail node value
+            case 'Z':
+              getTail(list);
+              break;
+            // remove head node
+            case 'T':
+              popHead(list);
+              break;
+            // remove tail node
+            case 'K':
+              popTail(list);
+              break;
+            // print size of list
+            case 'N':
+              getSize(list);
+              break;
+            // print list contents
+            case 'P':
+              printList(list);
+              break;
+            // commented line in input file
+            case '#':
+              break;
+            // unknown command in file
+            default:
+              cerr << "Invalid command detected in input file." << endl;
+              break;
+          }
+        } catch (const exception &e) {
+          cerr << e.what() << endl;
         }
       }
     }
@@ -175,13 +179,9 @@ private:
       int pos = input.find(" ");
       string stringvalue = input.substr(pos + 1);
       stringvalue = rmCarriageReturn(stringvalue);
-      try {
-        int value = stringToInt(stringvalue);
-        list->insertSorted(value);
-        cout << "VALUE " << value << " INSERTED" << endl;
-      } catch (const exception &e) {
-        cerr << e.what() << endl;
-      }
+      int value = stringToInt(stringvalue);
+      list->insertSorted(value);
+      cout << "VALUE " << value << " INSERTED" << endl;
     }
     else {
       cout << "MUST CREATE LIST INSTANCE" << endl;
@@ -194,13 +194,9 @@ private:
       int pos = input.find(" ");
       string stringvalue = input.substr(pos + 1);
       stringvalue = rmCarriageReturn(stringvalue);
-      try {
-        int value = stringToInt(stringvalue);
-        list->insertFront(value);
-        cout << "VALUE " << value << " ADDED TO HEAD" << endl;
-      } catch (const exception &e) {
-        cerr << e.what() << endl;
-      }
+      int value = stringToInt(stringvalue);
+      list->insertFront(value);
+      cout << "VALUE " << value << " ADDED TO HEAD" << endl;
     }
     else {
       cout << "MUST CREATE LIST INSTANCE" << endl;
@@ -213,13 +209,9 @@ private:
       int pos = input.find(" ");
       string stringvalue = input.substr(pos + 1);
       stringvalue = rmCarriageReturn(stringvalue);
-      try {
-        int value = stringToInt(stringvalue);
-        list->insertBack(value);
-        cout << "VALUE " << value << " ADDED TO TAIL" << endl;
-      } catch (const exception &e) {
-        cerr << e.what() << endl;
-      }
+      int value = stringToInt(stringvalue);
+      list->insertBack(value);
+      cout << "VALUE " << value << " ADDED TO TAIL" << endl;
     }
     else {
       cout << "MUST CREATE LIST INSTANCE" << endl;
@@ -232,17 +224,13 @@ private:
       int pos = input.find(" ");
       string stringvalue = input.substr(pos + 1);
       stringvalue = rmCarriageReturn(stringvalue);
-      try {
-        int value = stringToInt(stringvalue);
-        bool eliminated = list->eliminateAllOf(value);
-        if (eliminated) {
-          cout << "VALUE " << value << " ELIMINATED" << endl;
-        }
-        else {
-          cout << "VALUE " << value << " NOT FOUND" << endl;
-        }
-      } catch (const exception &e) {
-        cerr << e.what() << endl;
+      int value = stringToInt(stringvalue);
+      bool eliminated = list->eliminateAllOf(value);
+      if (eliminated) {
+        cout << "VALUE " << value << " ELIMINATED" << endl;
+      }
+      else {
+        cout << "VALUE " << value << " NOT FOUND" << endl;
       }
     }
     else {
@@ -256,17 +244,13 @@ private:
       int pos = input.find(" ");
       string stringvalue = input.substr(pos + 1);
       stringvalue = rmCarriageReturn(stringvalue);
-      try {
-        int value = stringToInt(stringvalue);
-        bool eliminated = list->eliminateFirstOf(value);
-        if (eliminated) {
-          cout << "VALUE " << value << " REMOVED" << endl;
-        }
-        else {
-          cout << "VALUE " << value << " NOT FOUND" << endl;
-        }
-      } catch (const exception &e) {
-        cerr << e.what() << endl;
+      int value = stringToInt(stringvalue);
+      bool eliminated = list->eliminateFirstOf(value);
+      if (eliminated) {
+        cout << "VALUE " << value << " REMOVED" << endl;
+      }
+      else {
+        cout << "VALUE " << value << " NOT FOUND" << endl;
       }
     }
     else {
@@ -280,16 +264,12 @@ private:
       int pos = input.find(" ");
       string stringvalue = input.substr(pos + 1);
       stringvalue = rmCarriageReturn(stringvalue);
-      try {
-        int value = stringToInt(stringvalue);
-        if (list->get(value)) {
-          cout << "VALUE " << value << " FOUND" << endl;
-        }
-        else {
-          cout << "VALUE " << value << " NOT FOUND" << endl;
-        }
-      } catch (const exception &e) {
-        cerr << e.what() << endl;
+      int value = stringToInt(stringvalue);
+      if (list->get(value)) {
+        cout << "VALUE " << value << " FOUND" << endl;
+      }
+      else {
+        cout << "VALUE " << value << " NOT FOUND" << endl;
       }
     }
     else {
