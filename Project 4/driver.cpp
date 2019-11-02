13cdbe355f5c5455ae9b293fe9790cf19f955f2c
klandrith007@gmail.com <klandrith007@gmail.com>
2019-11-01 20:35:41 -0700

    Reconfigured driver.cpp and added inputoutput.hpp to handle I/O

--- Project 4/driver.cpp
+++ Project 4/driver.cpp
@@ -1,5 +1,5 @@
 /*  Programmer:       Kyle Landrith
-    Dated Completed:  10/22/19
+    Dated Completed:  11/1/19
     Resources:        Geeks for Geeks, Stack Overflow, Tutorialspoint
                       https://stackoverflow.com/questions/2528995/remove-r-from-a-string-in-c
 
@@ -13,6 +13,7 @@
 #include <string>
 #include <fstream>
 #include "bstree.hpp"
+#include "inputoutput.hpp"
 #include "word.h"
 
 using std::string;
@@ -20,22 +21,17 @@ using std::cout;
 using std::endl;
 using std::ifstream;
 using std::cerr;
-using std::remove;
-
-// function declarations
-void InputOutput(string filename);
-string rmCarriageReturn(string input);
 
 // main that accepts a single command line argument for a filename
 int main (int argc, char *argv[]) {
   ifstream inFile(argv[1]);
   // error checking for command line arguments and successful file opening
   if (argc == 1) {
-    cerr << "Error: No file name entered. USAGE: list [filename]" << endl;
+    cerr << "Error: No file name entered. USAGE: driver [filename]" << endl;
     return 1;
   }
   if (argc >= 3) {
-    cerr << "Too many arguments passed. USAGE: list [filename]" << endl;
+    cerr << "Too many arguments passed. USAGE: driver [filename]" << endl;
     return 1;
   }
   if (!inFile.good()) {
@@ -46,199 +42,10 @@ int main (int argc, char *argv[]) {
   //close input file
   inFile.close();
 
-  // call operations function and pass in filename
-  InputOutput(argv[1]);
+  // create InputOutput class instance and call IO function
+  // and pass in filename
+  InputOutput io;
+  io.IOFunction(argv[1]);
 
   return 0;
-}
-
-// InputOutput handles parsing the input file and calling BSTree/Word functions
-void InputOutput(string filename) {
-  // create instance of BSTree and variables for input
-  BSTree<Word> *tree;
-  tree = nullptr;
-  string input;
-  char command;
-  ifstream inFile(filename);
-
-  // while loop for parsing commands and performing respective functions
-  while (!inFile.eof()) {
-    getline(inFile, input);
-    // only execute block if current line in file is not empty string
-    if (input.size() > 0) {
-      command = input.at(0);
-      // switch block for various command inputs to execute related functions
-      // in tree
-      switch (command) {
-        // create tree
-        case 'C':
-          if (tree != nullptr) {
-            delete tree;
-          }
-          tree = new BSTree<Word>;
-          cout << "TREE CREATED" << endl;
-          break;
-        // clear tree
-        case 'X':
-          if (tree != nullptr) {
-            tree->clear();
-            cout << "TREE CLEARED" << endl;
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // delete tree
-        case 'D':
-          if (tree != nullptr) {
-            delete tree;
-            tree = nullptr;
-            cout << "TREE DELETED" << endl;
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // insert node into tree
-        case 'I':
-          if (tree != nullptr) {
-            int pos = input.find(" ");
-            if (pos != string::npos) {
-              string stringinput = input.substr(pos + 1);
-              stringinput = rmCarriageReturn(stringinput);
-              if (tree->insert(stringinput)) {
-                cout << "WORD " << stringinput << " INSERTED" << endl;
-              }
-              else {
-                Word* w = tree->get(stringinput);
-                w->incrementCount();
-                cout << "WORD " << stringinput << " INCREMENTED" << endl;
-              }
-            }
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // find node in tree
-        case 'F':
-          if (tree != nullptr) {
-            int pos = input.find(" ");
-            if (pos != string::npos) {
-              string stringinput = input.substr(pos + 1);
-              stringinput = rmCarriageReturn(stringinput);
-              if (tree->getSize() == 0) {
-                cout << "TREE EMPTY" << endl;
-              }
-              else if (tree->find(stringinput)) {
-                cout << "FOUND " << stringinput << endl;
-              }
-              else {
-                cout << stringinput << " NOT FOUND" << endl;
-              }
-            }
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // remove node in tree
-        case 'R':
-          if (tree != nullptr) {
-            int pos = input.find(" ");
-            if (pos != string::npos) {
-              string stringinput = input.substr(pos + 1);
-              stringinput = rmCarriageReturn(stringinput);
-              if (tree->getSize() == 0) {
-                cout << "TREE EMPTY" << endl;
-              }
-              else if (tree->remove(stringinput)) {
-                cout << "REMOVED " << stringinput << endl;
-              }
-              else {
-                cout << stringinput << " NOT FOUND" << endl;
-              }
-            }
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // get node from tree (memory address)
-        case 'G':
-          if (tree != nullptr) {
-            int pos = input.find(" ");
-            if (pos != string::npos) {
-              string stringinput = input.substr(pos + 1);
-              stringinput = rmCarriageReturn(stringinput);
-              if (tree->getSize() == 0) {
-                cout << "TREE EMPTY" << endl;
-              }
-              else if (tree->find(stringinput)) {
-                Word* w = tree->get(stringinput);
-                int count = w->getCount();
-                cout << "GOT " << w->outputWord()<< " " << w->getCount() << endl;
-              }
-              else {
-                cout << stringinput << " NOT FOUND" << endl;
-              }
-            }
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // output size of tree
-        case 'N':
-          if (tree != nullptr) {
-            cout << "TREE SIZE IS " << tree->getSize() << endl;
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // print tree in ascending order
-        case 'O':
-          if (tree != nullptr) {
-            if (tree->getSize() != 0) {
-              tree->printInOrder();
-            }
-            else {
-              cout << "TREE EMPTY" << endl;
-            }
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // print tree in descending order
-        case 'E':
-          if (tree != nullptr) {
-            if (tree->getSize() != 0) {
-              tree->printReverseOrder();
-            }
-            else {
-              cout << "TREE EMPTY" << endl;
-            }
-          }
-          else {
-            cout << "MUST CREATE TREE INSTANCE" << endl;
-          }
-          break;
-        // ignore as comment (not needed, only for clarification)
-        case '#':
-          break;
-        // invalid command passed
-        default:
-        cerr << "Invalid input detected in input file." << endl;
-        break;
-      }
-    }
-  }
-}
-
-// rmCarriageReturn removes '\r' from any input for POSIX compaitibility
-string rmCarriageReturn(string input) {
-  input.erase(remove(input.begin(), input.end(), '\r'), input.end());
-  return input;
 }
---
+++ Project 4/inputoutput.hpp
@@ -0,0 +1,293 @@
+/*  Programmer:     Kyle Landrith
+    Date Completed: 11/1/19
+    Resources: None
+    Class declaration and implementation for an inputoutput function
+    to take in a filename and handle the IO for a binary search tree
+    program.
+
+    OPERATION codes + descriptions
+
+    CODE [argument] : description
+
+    # any text      : ignore as comment
+    C               : create dynamic tree instance
+    X               : clear the current tree instance of contents
+    D               : delete the dynamic tree instance and set to nullptr
+    I word          : insert word (string) into the tree
+    F word          : find word (string) in the tree
+    R word          : remove word (string) from the tree
+    G word          : get word (string) from the tree
+    N               : return the size (number of nodes) of the tree
+    O               : in order print
+    E               : reverse order print
+
+*/
+
+#pragma once
+
+#include <iostream>
+#include "bstree.hpp"
+#include "word.h"
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::cerr;
+using std::exception;
+using std::remove;
+
+class InputOutput {
+public:
+  // InputOutput handles parsing the input file and calling DList functions
+  void IOFunction(string filename) {
+    // create a list pointer and other variables
+    BSTree<Word> *tree = nullptr;
+    string input;
+    char command;
+    ifstream inFile(filename);
+
+    // while loop for parsing commands and calling respective functions in DList
+    while (!inFile.eof()) {
+      getline(inFile, input);
+      // only execute block if current line in file is not empty string
+      if (input.length() > 0) {
+        command = input.at(0);
+        try {
+          // switch block for various command inputs to execute related functions
+          // in BSTree
+          switch (command) {
+            // create tree instance
+            case 'C':
+              create(tree);
+              break;
+            // clear current tree instance of nodes
+            case 'X':
+              clear(tree);
+              break;
+            // delete current tree instance
+            case 'D':
+              deletetree(tree);
+              break;
+            // insert input into the tree
+            case 'I':
+              insert(input, tree);
+              break;
+            // insert number at front of list
+            case 'F':
+              find(input, tree);
+              break;
+            // remove first instance of a number
+            case 'R':
+              removeNode(input, tree);
+              break;
+            // find a value in list
+            case 'G':
+              get(input, tree);
+              break;
+            // print size of tree
+            case 'N':
+              getSize(tree);
+              break;
+            // print list contents
+            case 'O':
+              printInOrder(tree);
+              break;
+            case 'E':
+              printReverseOrder(tree);
+              break;
+            // commented line in input file
+            case '#':
+              break;
+            // unknown command in file
+            default:
+              cerr << "Invalid command detected in input file." << endl;
+              break;
+          }
+        } catch (const exception &e) {
+          cerr << e.what() << endl;
+        }
+      }
+    }
+    // close file
+    inFile.close();
+  }
+private:
+  // create function to create a new tree instance
+  void create(BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      delete tree;
+    }
+    tree = new BSTree<Word>;
+    cout << "TREE CREATED" << endl;
+  }
+
+  // clear clears the current tree instance of nodes
+  void clear(BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      tree->clear();
+      cout << "TREE CLEARED" << endl;
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // deletetree deletes the current tree instance and sets
+  // tree to nullptr
+  void deletetree(BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      delete tree;
+      tree = nullptr;
+      cout << "TREE DELETED" << endl;
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // insert inserts a new node into the tree with the value
+  // in via input
+  void insert(string input, BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      int pos = input.find(" ");
+      if (pos != string::npos) {
+        string stringinput = input.substr(pos + 1);
+        stringinput = rmCarriageReturn(stringinput);
+        if (tree->insert(stringinput)) {
+          cout << "WORD " << stringinput << " INSERTED" << endl;
+        }
+        else {
+          Word* w = tree->get(stringinput);
+          w->incrementCount();
+          cout << "WORD " << stringinput << " INCREMENTED" << endl;
+        }
+      }
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // find searchs the tree for the value passed in via input
+  void find(string input, BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      int pos = input.find(" ");
+      if (pos != string::npos) {
+        string stringinput = input.substr(pos + 1);
+        stringinput = rmCarriageReturn(stringinput);
+        if (tree->getSize() == 0) {
+          cout << "TREE EMPTY" << endl;
+        }
+        else if (tree->find(stringinput)) {
+          cout << "FOUND " << stringinput << endl;
+        }
+        else {
+          cout << stringinput << " NOT FOUND" << endl;
+        }
+      }
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // remove removes a node from the tree if there is a node
+  // that matches the value passed in via input
+  void removeNode(string input, BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      int pos = input.find(" ");
+      if (pos != string::npos) {
+        string stringinput = input.substr(pos + 1);
+        stringinput = rmCarriageReturn(stringinput);
+        if (tree->getSize() == 0) {
+          cout << "TREE EMPTY" << endl;
+        }
+        else if (tree->remove(stringinput)) {
+          cout << "REMOVED " << stringinput << endl;
+        }
+        else {
+          cout << stringinput << " NOT FOUND" << endl;
+        }
+      }
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // getNode searchs the tree for the value passed in via input and
+  // outputs the node value
+  void get(string input, BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      int pos = input.find(" ");
+      if (pos != string::npos) {
+        string stringinput = input.substr(pos + 1);
+        stringinput = rmCarriageReturn(stringinput);
+        if (tree->getSize() == 0) {
+          cout << "TREE EMPTY" << endl;
+        }
+        else if (tree->find(stringinput)) {
+          Word* w = tree->get(stringinput);
+          int count = w->getCount();
+          cout << "GOT " << w->outputWord()<< " " << w->getCount() << endl;
+        }
+        else {
+          cout << stringinput << " NOT FOUND" << endl;
+        }
+      }
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // getSize outputs the current size of the tree (number of nodes)
+  void getSize(BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      cout << "TREE SIZE IS " << tree->getSize() << endl;
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // printInOrder prints the contents of the tree in ascending order
+  void printInOrder(BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      if (tree->getSize() != 0) {
+        tree->printInOrder();
+      }
+      else {
+        cout << "TREE EMPTY" << endl;
+      }
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // printReverseOrder prints the contents of the tree in descending order
+  void printReverseOrder(BSTree<Word> *&tree) {
+    if (tree != nullptr) {
+      if (tree->getSize() != 0) {
+        tree->printReverseOrder();
+      }
+      else {
+        cout << "TREE EMPTY" << endl;
+      }
+    }
+    else {
+      cout << "MUST CREATE TREE INSTANCE" << endl;
+    }
+  }
+
+  // rmCarriageReturn removes '\r' from any input for POSIX compaitibility
+  string rmCarriageReturn(string input) {
+    input.erase(remove(input.begin(), input.end(), '\r'), input.end());
+    return input;
+  }
+};
